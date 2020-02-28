acc5dea591252924b9865b37535c6bdaee10bf61
Vlad Demchenko <vdemchenko99@gmail.com>
2020-02-28 18:21:33 +0200

    refactor infrastucture for repositories

---
+++ src/Infrastructure/Data/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class DbContext : IdentityDbContext<IdentityUser>
+    {
+        public DbContext(DbContextOptions<DbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
