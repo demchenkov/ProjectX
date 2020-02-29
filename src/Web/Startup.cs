5066092e1b4d04f94b5031a5b80524726b6874e2
Vedaukr <kalipso121221@gmail.com>
2020-02-29 15:05:31 +0200

    Added swagger to app configuration.

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Web.Middleware.ExtensionMethods;
 using DbContext = Infrastructure.DbContext;
 
@@ -35,6 +36,11 @@ namespace Web
             services.AddControllers().AddNewtonsoftJson();
 
             services.AddTransient<ILocationRepository, LocationRepository>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -42,7 +48,15 @@ namespace Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                
+                app.UseSwagger();
+
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                });
             }
+
             app.UseErrorHandling();
 
             app.UseRouting();
